6fc071192717053d50163a49681d34ee49973db3
LiteHell <litehell@litehell.info>
2023-07-26 04:03:51 +0900

    Close dialog when background wrapper is clicked

--- src/header/gpgDialog/wrapper.tsx
+++ src/header/gpgDialog/wrapper.tsx
@@ -8,9 +8,13 @@ type Options = {
 }
 
 export default function Wrapper({ active, onCloseButtonClick, children }: Options) {
-    const wrapperClasses = ["fixed top-0 left-0 w-screen h-screen flex flex-col content-center bg-black bg-opacity-75 justify-center"];
+    const wrapperClasses = ["fixed top-0 left-0 w-screen h-screen flex flex-col content-center bg-black bg-opacity-75 justify-center gpg-dialog-wrapper"];
     if (!active) wrapperClasses.push('hidden');
-    return <div className={wrapperClasses.join(' ')}>
+    return <div className={wrapperClasses.join(' ')} onClickCapture={(evt) => {
+        if (evt.target !== evt.currentTarget)
+            return;
+        onCloseButtonClick();
+    }}>
         <div className='m-4 p-2 bg-slate-300 shadow rounded overflow-y-scroll md:mx-48'>
             <div className='flex flex-row-reverse'>
                 <a onClick={onCloseButtonClick} className="cursor-pointer">
