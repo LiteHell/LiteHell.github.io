389a177a6f32e01b1f7b11b28505afff2dd58dfd
LiteHell <litehell@litehell.info>
2023-07-30 00:29:56 +0900

    Use dynamic viewport

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -3,7 +3,7 @@ import BackgroundImage from './backgroundImage';
 import Content, { HeaderContentOptions } from './content';
 
 export default function Header(opts: HeaderContentOptions): ReactElement {
-    return <header className="h-screen w-screen flex flex-col md:flex-row-reverse shadow-md">
+    return <header className="h-dynamic-screen w-dynamic-screen flex flex-col md:flex-row-reverse shadow-md">
         <BackgroundImage></BackgroundImage>
         <Content
             {...opts}
