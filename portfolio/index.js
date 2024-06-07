0021b69b3f8caa7271c342c0581f54bf4e8075d1
LiteHell <litehell@litehell.info>
2024-06-07 15:29:01 +0000

    Deploying to gh-pages from  @ 41de0735cc818ca85c2e0be3e973eaa6fa6b2cb3 🚀

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,7 +3,7 @@ toggleLink.addEventListener('click', (evt) => {
     evt.preventDefault();
     let html = document.documentElement;
     if (html.classList.contains('minor-projects-hidden')) {
-        toggleLink.textContent = '모든 프로젝트가 표시됩니다. 중요한 프로젝트를 표시하려면 여기를 누르세요.';
+        toggleLink.textContent = '모든 프로젝트가 표시됩니다. 중요한 프로젝트만 표시하려면 여기를 누르세요.';
         html.classList.remove('minor-projects-hidden');
     } else {
         toggleLink.textContent = '중요한 프로젝트만 표시됩니다. 모든 프로젝트를 표시하려면 여기를 누르세요.';
